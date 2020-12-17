4010ab56e64996f787c0a61c9e65f2a6d59a83f3
TONY <tonnymusyoka@gmail.com>
2020-12-17 11:31:28 +0300

    Add project files.

---
+++ FileIOClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using FileIOLibrary;
+
+namespace FileIOClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "ReadFile.txt";
+            int lineNumber = 1;
+
+            string lineContents = null;
+
+            try
+            {
+                FileIO fileIO = new FileIO();
+                lineContents = fileIO.ReadLine(filePath, lineNumber);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("There was an error reading the file: ");
+                Console.WriteLine(e.Message);
+            }
+
+            if (lineContents != null) {
+                Console.WriteLine(lineContents);
+                Console.ReadLine();
+            }
+               
+        }
+    }
+}
---
+++ FileIOLibrary/FileIO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOLibrary
+{
+    public class FileIO
+    {
+        public string ReadLine(string filePath, int lineNumber)
+        {
+            return ReadSpecificLine(filePath, lineNumber);
+        }
+
+        private string ReadSpecificLine(string filePath, int lineNumber)
+        {
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                string content = null;
+                for (int i = 1; i < lineNumber; i++)
+                {
+                    file.ReadLine();
+
+                    if (file.EndOfStream)
+                    {
+                        break;
+                    }
+                }
+                content = file.ReadLine();
+                return content;
+            }
+        }
+    }
+}
